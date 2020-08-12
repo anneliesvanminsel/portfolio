1112c46658e54207fdc7d0f711033c5b8db2dde3
Annelies <anneliesvanminsel@outlook.com>
2020-08-12 10:55:59 +0200

    Added hover effect on buttons

--- portfolio/resources/views/partials/socials.blade.php
+++ portfolio/resources/views/partials/socials.blade.php
@@ -1,14 +1,14 @@
 <div class="socials row">
-	<a href="https://github.com/anneliesvanminsel">
+	<a href="https://github.com/anneliesvanminsel" class="button is-icon">
 		@svg('github')
 	</a>
-	<a href="https://www.linkedin.com/in/annelies-van-minsel-7a7504102/">
+	<a href="https://www.linkedin.com/in/annelies-van-minsel-7a7504102/" class="button is-icon">
 		@svg('linkedin')
 	</a>
-	<a href="https://www.instagram.com/liesjevanminsel/">
+	<a href="https://www.instagram.com/liesjevanminsel/" class="button is-icon">
 		@svg('instagram')
 	</a>
-	<a href="https://twitter.com/Liesjevanminsel">
-		@svg('twitter')
+	<a href="https://twitter.com/Liesjevanminsel" class="button is-icon">
+		@svg('twitter', 'is-link')
 	</a>
 </div>
