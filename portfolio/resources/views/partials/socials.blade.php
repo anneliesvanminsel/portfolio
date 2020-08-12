a4f4b0d021a9020e02d4f0cb8a8de71f639fdc7f
Annelies <anneliesvanminsel@outlook.com>
2020-08-12 11:17:06 +0200

    added aria labels to icons

--- portfolio/resources/views/partials/socials.blade.php
+++ portfolio/resources/views/partials/socials.blade.php
@@ -1,14 +1,30 @@
 <div class="socials row">
-	<a href="https://github.com/anneliesvanminsel" class="button is-icon">
+	<a
+		href="https://github.com/anneliesvanminsel"
+		class="button is-icon"
+		aria-label="Link to Annelies' Github-profile"
+	>
 		@svg('github')
 	</a>
-	<a href="https://www.linkedin.com/in/annelies-van-minsel-7a7504102/" class="button is-icon">
+	<a
+		href="https://www.linkedin.com/in/annelies-van-minsel-7a7504102/"
+		class="button is-icon"
+		aria-label="Link to Annelies' LinkedIn-profile"
+	>
 		@svg('linkedin')
 	</a>
-	<a href="https://www.instagram.com/liesjevanminsel/" class="button is-icon">
+	<a
+		href="https://www.instagram.com/liesjevanminsel/"
+		class="button is-icon"
+		aria-label="Link to Annelies' Instagram-profile"
+	>
 		@svg('instagram')
 	</a>
-	<a href="https://twitter.com/Liesjevanminsel" class="button is-icon">
+	<a
+		href="https://twitter.com/Liesjevanminsel"
+		class="button is-icon"
+		aria-label="Link to Annelies' Twitter-profile"
+	>
 		@svg('twitter', 'is-link')
 	</a>
 </div>
